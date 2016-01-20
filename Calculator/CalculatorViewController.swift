a180b75d16ed08ccfe288b06ec31bba1d890c04c
Mert Serin <mert-serin@outlook.com>
2016-01-20 16:13:33 +0200

    Paul Hegarty's part

--- Calculator/CalculatorViewController.swift
+++ Calculator/CalculatorViewController.swift
@@ -12,7 +12,7 @@ class CalculatorViewController: UIViewController {
     
     @IBOutlet weak var display: UILabel!
     
-    var middleOfTyping:Bool = false
+    var middleOfTyping = false
     
     
     
@@ -25,10 +25,57 @@ class CalculatorViewController: UIViewController {
             display.text! = digit
             middleOfTyping = true
         }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if(middleOfTyping){
+            enter()
+        }
         
+        switch operation{
+        case "+": performOperation{$0 + $1}
+        case "x": performOperation{$0 * $1}
+        case "-": performOperation{$0 - $1}
+        case "÷": performOperation{$1 / $0}
+        case "√": performOperation{sqrt($0)}
+        default:break
+        }
         
     }
     
+    func performOperation(operation: (Double, Double) -> Double){
+        if(operandStack.count >= 2){
+        displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+        enter()
+        }
+    }
+    @nonobjc
+    func performOperation(operation: Double -> Double){
+        if(operandStack.count >= 1){
+        displayValue = operation(operandStack.removeLast())
+        enter()
+        }
+    }
+    
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        operandStack.append(Double(displayValue))
+        middleOfTyping = false
+        print(operandStack)
+    }
+    
+    
+    var displayValue: Double {
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = "\(newValue)"
+            middleOfTyping = false
+        }
+    }
     
 
 
